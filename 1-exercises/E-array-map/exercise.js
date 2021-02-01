b374da1d7e5752ca1c438f25b4c48829e01e3fe2
Murat Demirtas <73036794+muratdemirtas2001@users.noreply.github.com>
2021-02-01 20:30:26 +0000

    finalizing 1-E-array-map exercise

--- 1-exercises/E-array-map/exercise.js
+++ 1-exercises/E-array-map/exercise.js
@@ -2,3 +2,23 @@
 // Write multiple solutions using different syntax (as shown in the README)
 
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+// 1-using named function
+// const multipliedNumbers = numbers.map(function multplyby100(number) {
+//   return number * 100;
+// });
+
+// 2- using anonymous function
+// const multipliedNumbers = numbers.map(function (number) {
+//   return number * 100;
+// });
+
+// 3- using arrow function
+// const multipliedNumbers = numbers.map((number)=> {
+//   return number * 100;
+// });
+
+// 4- using arrow function with implicit return
+
+const multipliedNumbers = numbers.map((number)=> number * 100);
+
+console.log(multipliedNumbers);
