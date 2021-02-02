3edb2584bb95c2463a5951128ab76e037dcc5ebf
Murat Demirtas <73036794+muratdemirtas2001@users.noreply.github.com>
2021-02-02 11:39:49 +0000

    finalizing mandatory bush-berries

--- 2-mandatory/2-bush-berries.js
+++ 2-mandatory/2-bush-berries.js
@@ -21,7 +21,12 @@
   Let's first look at an example that will teach you how to use these methods.
 */
 
-function bushChecker() {}
+function bushChecker(bushes) {
+  const isAllPink = bushes.every((bush) => {
+    return bush === "pink";
+  });
+  return isAllPink ? "Bush is safe to eat from" : "Toxic! Leave bush alone!";
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
